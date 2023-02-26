d89db49a779df2a4653d62576a57654e9c7d8849
AbdulazizMabrouk <abdulaziz.maqa@gmail.com>
2023-02-26 11:29:45 +0200

    handle comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const webp = require("gulp-webp");
 gulp.task("default", () =>
+  // we can change the extension from here  gulp.src("src/images/*.png")
   gulp.src("src/images/*.png").pipe(webp()).pipe(gulp.dest("dist/images"))
 );
