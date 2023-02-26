6806e83a6641bff110fc55f4bcd68b861f719985
AbdulazizMabrouk <abdulaziz.maqa@gmail.com>
2023-02-26 11:25:14 +0200

    add git ignor

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,5 @@
 const gulp = require("gulp");
 const webp = require("gulp-webp");
-// const imagemin = require("gulp-imagemin");
 gulp.task("default", () =>
-  gulp.src(["src/images/*.png]"]).pipe(webp()).pipe(gulp.dest("dist/images"))
+  gulp.src("src/images/*.png").pipe(webp()).pipe(gulp.dest("dist/images"))
 );
-
-// gulp.task("compress-images", function () {
-//   return gulp
-//     .src("src/images/*")
-//     .pipe(imagemin())
-//     .pipe(gulp.dest("dist/images"));
-// });
